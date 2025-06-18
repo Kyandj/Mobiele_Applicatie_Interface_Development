b1dbeccd78fde9c20afd7a10c97879cde88eb7a9
Kyandj <2402586jong@zuyd.nl>
2025-06-18 14:09:44 +0200

    Pagina's aangemaakt (API moet nog gekoppeld worden)

---
+++ Mobiele_Applicatie_Interface_Studio/Pagina's/BestellingDetails.xaml.cs
@@ -0,0 +1,9 @@
+namespace Mobiele_Applicatie_Interface_Studio.Pagina_s;
+
+public partial class BestellingDetails : ContentPage
+{
+	public BestellingDetails()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mobiele_Applicatie_Interface_Studio/Pagina's/RoutePagina.xaml.cs
@@ -0,0 +1,9 @@
+namespace Mobiele_Applicatie_Interface_Studio.Pagina_s;
+
+public partial class RoutePagina : ContentPage
+{
+	public RoutePagina()
+	{
+		InitializeComponent();
+	}
+}
