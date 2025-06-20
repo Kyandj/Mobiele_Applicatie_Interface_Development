49d85f940400e6379ef452227c4607e210c32c1a
Duncan Schmets <2404512schmets@zuyd.nl>
2025-06-20 12:33:56 +0200

    BestellingDetails Pagina (Werkt niet)

---
+++ Mobiele_Applicatie_Interface_Studio/Models/Bestelling.cs
@@ -0,0 +1,7 @@
+public class Bestelling
+{
+    public string BestellingsId { get; set; }
+    public string Bestemming { get; set; }
+    public string Deadline { get; set; }
+    public string Status { get; set; }
+}
--- Mobiele_Applicatie_Interface_Studio/Pages/BestellingDetails.xaml.cs
+++ Mobiele_Applicatie_Interface_Studio/Pages/BestellingDetails.xaml.cs
@@ -1,18 +1,26 @@
+using Mobiele_Applicatie_Interface_Studio.Services;
+using Mobiele_Applicatie_Interface_Studio.ViewModels;
+
 namespace Mobiele_Applicatie_Interface_Studio.Pages;
 
 public partial class BestellingDetails : ContentPage
 {
-	public BestellingDetails()
-	{
-		InitializeComponent();
-	}
-
+    private BestellingDetailsViewModel _viewModel;
     private readonly LocationService _locationService = new();
+
+    public BestellingDetails()
+    {
+        InitializeComponent();
+        var service = new BestellingService(new HttpClient());
+        _viewModel = new BestellingDetailsViewModel(service);
+        BindingContext = _viewModel;
+    }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
         await _locationService.StartLiveTrackingAsync();
+        await _viewModel.LoadBestellingAsync("98361538014"); // Replace with actual ID
     }
 
     protected override void OnDisappearing()
@@ -20,5 +28,4 @@ public partial class BestellingDetails : ContentPage
         base.OnDisappearing();
         _locationService.StopLiveTracking();
     }
-
 }
