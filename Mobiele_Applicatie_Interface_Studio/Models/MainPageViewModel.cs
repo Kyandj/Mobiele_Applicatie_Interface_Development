1aac34d807ebfe27cca335a1271c5f6e70b41f79
Duncan Schmets <2404512schmets@zuyd.nl>
2025-06-24 10:12:32 +0200

    Spacing Status Button & TImewindows Orders

--- Mobiele_Applicatie_Interface_Studio/Models/MainPageViewModel.cs
+++ Mobiele_Applicatie_Interface_Studio/Models/MainPageViewModel.cs
@@ -18,20 +18,20 @@ public class MainPageViewModel
 
             //Order worden weergeven zoals ze hier onder elkaar staan
 
-            new Order { OrderId = 607857972 , Address = "Drievogelstraat 128\n6466KW", TimeWindow = "12:00 - 14:00" ,BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
-            new Order { OrderId = 256237835, Address = "Kerkstraat 22\n6467AB", TimeWindow = "09:00 - 11:00" ,BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
+            new Order { OrderId = 607857972 , Address = "Drievogelstraat 128\n6466KW", TimeWindow = "10:00 - 12:00" ,BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
+            new Order { OrderId = 256237835, Address = "Kerkstraat 22\n6467AB", TimeWindow = "12:00 - 14:00" ,BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
             new Order { OrderId = 372226621, Address = "Stationsweg 5\n6468CD", TimeWindow = "14:00 - 16:00" , BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
             new Order { OrderId = 089808658, Address = "Marktplein 1\n6469EF", TimeWindow = "16:00 - 18:00" , BezorgingStatus = "Onderweg", OrderKleur = Colors.Yellow},
 
-            new Order { OrderId = 497363565, Address = "Willembeekmanstraat 6\n6271CZ", TimeWindow = "18:00 - 20:00" , BezorgingStatus = "Niet Thuis", OrderKleur = Colors.Red},
-            new Order { OrderId = 948095675, Address = "Bergweg 10\n6272GH", TimeWindow = "10:00 - 12:00" , BezorgingStatus = "Niet Thuis", OrderKleur = Colors.Red},
+            new Order { OrderId = 497363565, Address = "Willembeekmanstraat 6\n6271CZ", TimeWindow = "10:00 - 14:00" , BezorgingStatus = "Niet Thuis", OrderKleur = Colors.Red},
+            new Order { OrderId = 948095675, Address = "Bergweg 10\n6272GH", TimeWindow = "14:00 - 12:00" , BezorgingStatus = "Niet Thuis", OrderKleur = Colors.Red},
 
             new Order { OrderId = 590614625, Address = "Dorpsstraat 77\n6273IJ", TimeWindow = "08:00 - 09:00" , BezorgingStatus = "Bezorgd", OrderKleur = Colors.Green},
             new Order { OrderId = 750145170, Address = "Schoollaan 3\n6274KL", TimeWindow = "11:00 - 12:00" , BezorgingStatus = "Bezorgd", OrderKleur = Colors.Green},
             new Order { OrderId = 345489437, Address = "Parklaan 8\n6275MN", TimeWindow = "13:00 - 14:00" , BezorgingStatus = "Bezorgd", OrderKleur = Colors.Green},
 
-            new Order { OrderId = 881916491, Address = "Willembeekmanstraat 6\n6271CZ", TimeWindow = "18:00 - 20:00" , BezorgingStatus = "Overig", OrderKleur = Colors.Cyan},
-            new Order { OrderId = 112381910, Address = "Bergweg 10\n6272GH", TimeWindow = "10:00 - 12:00" , BezorgingStatus = "Overig", OrderKleur = Colors.Cyan},
+            new Order { OrderId = 881916491, Address = "Willembeekmanstraat 6\n6271CZ", TimeWindow = "9:00 - 11:00" , BezorgingStatus = "Overig", OrderKleur = Colors.Cyan},
+            new Order { OrderId = 112381910, Address = "Bergweg 10\n6272GH", TimeWindow = "12:00 - 16:00" , BezorgingStatus = "Overig", OrderKleur = Colors.Cyan},
 
         };
     }
